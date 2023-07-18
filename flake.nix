048e74b005ceaabce788449e395e7ca9c915a8b4
mislavzanic <mislavzanic3@gmail.com>
2023-07-18 21:27:09 +0200

    add hm module

--- flake.nix
+++ flake.nix
@@ -1,9 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
+    home-manager = {
+      url = "github:rycee/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs @ {nixpkgs, ...}: {
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    flake-utils,
+    ...
+  }: let
     mkLib = attrs @ {pkgs, inputs, ...}: (self: super: {
       my = import ./lib {
         inherit pkgs inputs;
@@ -11,10 +21,17 @@
       };
     });
 
-    mkPkgs = attrs @ {pkgs, overlays, system, ...}:
+    mkPkgs = attrs @ {pkgs, overlays ? [], system, ...}:
       import pkgs ({
         inherit system overlays;
         config.allowUnfree = true;
       } // attrs);
-  };
+
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = mkPkgs {inherit system; pkgs = nixpkgs;};
+      lib = nixpkgs.lib.extend (mkLib {inherit pkgs inputs;});
+    in {
+      modules = mapModulesRec ./modules import;
+    }) // { inherit mkLib mkPkgs; };
 }
