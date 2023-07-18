3351b14f8164131b1fc80966485dd7e506936ac2
mislavzanic <mislavzanic3@gmail.com>
2023-07-19 00:57:03 +0200

    fix

--- modules/home.nix
+++ modules/home.nix
@@ -11,7 +11,7 @@ with lib.my; {
     user = mkOpt attrs {};
 
     dotfiles = {
-      dir = 
+      dir = mkOpt path
         (findFirst pathExists (toString ./.) [
           "${config.user.home}/.config/.dotfiles"
         ]);
